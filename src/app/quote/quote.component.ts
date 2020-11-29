cda68d133e1bc93fc859886d2286513553a5088b
Brian <bweloba@gmail.com>
2020-11-29 10:17:10 +0300

    add confirmation prompt

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,20 +8,24 @@ import { Quote } from '../quote';
 })
 export class QuoteComponent implements OnInit {
 
-  quotes:Quote[] = [
-    new Quote(1, 'In the fight between you and the world, back the world.', 'Franz Kafka',0),
-    new Quote(2 , 'He who has a why to live can bear almost any how.' ,'Friedrich Nietzsche',0 ),
-    new Quote(3 , 'One day, in retrospect, the years of struggle will strike you as the most beautiful.' ,'Sigmund Freud',0 ),
-    new Quote(4 , 'Show me a sane man and I will cure him for you.' ,'Carl Jung',0),
+  quotes: Quote[] = [
+    new Quote(1, 'In the fight between you and the world, back the world.', 'Franz Kafka', 0),
+    new Quote(2, 'He who has a why to live can bear almost any how.', 'Friedrich Nietzsche', 0),
+    new Quote(3, 'One day, in retrospect, the years of struggle will strike you as the most beautiful.', 'Sigmund Freud', 0),
+    new Quote(4, 'Show me a sane man and I will cure him for you.', 'Carl Jung', 0),
   ];
 
-  toggleDetails(index){
+  toggleDetails(index) {
     this.quotes[index].showDescription = !this.quotes[index].showDescription;
   }
 
-  quoteDelete(removeQuote, index){
-    if(removeQuote) {
-      this.quotes.splice(index,1);
+  quoteDelete(removeQuote, index) {
+    if (removeQuote) {
+      let toDelete = confirm('Are you sure you want to delete this quote?')
+
+      if (toDelete) {
+        this.quotes.splice(index, 1);
+      }
     }
   }
 
